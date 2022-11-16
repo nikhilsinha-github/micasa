e88a50eb259494d28c4e34d0f31fe93bd034d967
Nikhil Sinha <nikhilsinha.github@gmail.com>
2022-11-17 03:29:07 +0530

    our featured agents

--- src/components/AgentCard.js
+++ src/components/AgentCard.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { FaStar } from 'react-icons/fa';
+import { IoEllipsisVerticalOutline } from 'react-icons/io5';
+import '../styles/agent_card.css'
 
 export const AgentCard = () => {
   return (
-    <div className="agent-card">AgentCard</div>
+    <div className="agent-card">
+      <div className="icons">
+        <FaStar color="rgb(84,115,109)"/>
+        <IoEllipsisVerticalOutline color="rgb(84,115,109)"/>
+      </div>
+      <div className="agent-image">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-GhyeRCrZKqtN9dIZzm1cWi0kBuUTTlA3KI7jFS9k&s" alt="" />
+      </div>
+      <p className="agent-name">Muslim Uddin</p>
+      <p className="agent-designation">Manager</p>
+      <p className="agent-phone">+8801716-401415</p>
+      <p className="agent-email">homeway17@gmail.com</p>
+    </div>
   )
 }
--- src/components/FeaturedAgents.js
+++ src/components/FeaturedAgents.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import '../styles/featured_agents.css';
+import {AgentCard} from './AgentCard'
 
 export const FeaturedAgents = () => {
   return (
     <div className="featured-agents">
         <h2>Our Featured Agents</h2>
         <p>Find new & featured property for you</p>
+        <div className="agent-cards">
+            <AgentCard/>
+            <AgentCard/>
+            <AgentCard/>
+        </div>
     </div>
   )
 }
